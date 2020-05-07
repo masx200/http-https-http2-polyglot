90aa0661f7a65e7b9e568c2189ded5117ba4553d
masx200 <34191203+masx200@users.noreply.github.com>
2020-05-07 22:14:18 +0800

    整理

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,16 +1,12 @@
 /// <reference types="node" />
-import http from "http";
-import https from "https";
-import net from "net";
-import spdy from "spdy";
-import stream from "stream";
-import tls from "tls";
+import http from 'http';
+import https from 'https';
+import net from 'net';
+import spdy from 'spdy';
+import stream from 'stream';
+import tls from 'tls';
 
-export declare function createServer(
-    config: ServerOptions,
-    requestListener?: RequestListener,
-    upgradeListener?: UpgradeListener
-): net.Server;
+export declare function createServer(config: ServerOptions, requestListener?: RequestListener, upgradeListener?: UpgradeListener): net.Server;
 
 export declare interface PushOptions {
     status?: number;
@@ -19,22 +15,14 @@ export declare interface PushOptions {
     response?: http.OutgoingHttpHeaders;
 }
 
-export declare type RequestListener = (
-    req: ServerRequest,
-    res: ServerResponse
-) => void;
+export declare type RequestListener = (req: ServerRequest, res: ServerResponse) => void;
 
-export declare const requestNotFound: (
-    req: ServerRequest,
-    res: ServerResponse
-) => void;
+export declare const requestNotFound: (req: ServerRequest, res: ServerResponse) => void;
 
 export declare type ServerOptions = spdy.ServerOptions & {
     allowHalfOpen?: boolean | undefined;
     pauseOnConnect?: boolean | undefined;
-} & http.ServerOptions &
-    tls.TlsOptions &
-    https.ServerOptions;
+} & http.ServerOptions & tls.TlsOptions & https.ServerOptions;
 
 export declare interface ServerRequest extends http.IncomingMessage {
     socket: Socket;
@@ -45,18 +33,10 @@ export declare interface ServerResponse extends http.ServerResponse {
     push?: (pathname: string, options?: PushOptions) => stream.Writable;
 }
 
-export declare type Socket = Partial<tls.TLSSocket> & net.Socket;
+export declare type Socket = tls.TLSSocket | net.Socket;
 
-export declare type UpgradeListener = (
-    req: ServerRequest,
-    socket: Socket,
-    head: Buffer
-) => void;
+export declare type UpgradeListener = (req: ServerRequest, socket: Socket, head: Buffer) => void;
 
-export declare const upgradeNotFound: (
-    req: ServerRequest,
-    socket: Socket,
-    head: Buffer
-) => void;
+export declare const upgradeNotFound: (req: ServerRequest, socket: net.Socket | tls.TLSSocket, head: Buffer) => void;
 
-export {};
+export { }
