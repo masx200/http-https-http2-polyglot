b664b832a611bc9cde0c026d53c146c2f2339408
masx200 <34191203+masx200@users.noreply.github.com>
2020-09-13 22:51:06 +0800

    Update index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -121,9 +121,9 @@ tls.TLSSocket
             } else {
                 const response = [
                     `HTTP/1.1 400 Bad Request`,
-                    `content-type: text/html`,
-                    //   `Date: Fri, 08 May 2020 16:20:58 GMT`,
-                    `Connection: keep-alive`,
+                    
+                 
+                    
                     `Content-Length: 0`,
                     "",
                     "",
