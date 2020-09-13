657b354ca54179960ed88a85acdeb405e738ff0b
masx200 <34191203+masx200@users.noreply.github.com>
2020-09-13 22:11:37 +0800

    Update generate-ssl-cert.cmd

--- generate-ssl-cert.cmd
+++ generate-ssl-cert.cmd
@@ -1,1 +1,1 @@
-openssl req -x509  -days 365 -newkey rsa:2048 -nodes -sha256 -subj "/C=CN/ST=Shanghai/L=Shanghai/O=boyusc/OU=boyusc/CN=localhost" -keyout "server.key.pem" -out "server.crt.pem"
+openssl req -x509  -days 365 -newkey rsa:2048 -nodes -sha256 -subj "/C=CN/ST=Shanghai/L=Shanghai/O=localhost/OU=localhost/CN=localhost" -keyout "server.key.pem" -out "server.crt.pem"
